1074856e82243a9a9345f9da3bc26ae1822e727a
Levi Harris <levlevi@beowulf.cs.unc.edu>
2025-12-19 12:40:08 -0500

    hrrr var alignment

--- _copy.sh
+++ _copy.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+EXP_NAME="_copy_log"
 SRC="/playpen-hdd/levi/UNITES-backup-10-29-25/tianlong-chen-lab/nws-lv-precip-forecasting/ccrfcd-gauge-grids/data"
 DST="data"
 
-rsync -a --progress "$SRC"/ "$DST"/
+rsync -a --progress "$SRC"/ "$DST"/ > "$EXP_NAME.out" 2>&1 &
+
+# ---------------------------------------
+exit
