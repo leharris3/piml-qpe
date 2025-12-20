ca5507fd6c31dbc477c135f89c4afb7733c8ecd7
Levi Harris <levlevi@beowulf.cs.unc.edu>
2025-12-20 13:55:29 -0500

    hrrr var extraction @n @gauges

--- _copy.sh
+++ _copy.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-EXP_NAME="_copy_log"
+EXP_NAME="_extract_hrrr_vars_at_gauges"
 SRC="/playpen-hdd/levi/UNITES-backup-10-29-25/tianlong-chen-lab/nws-lv-precip-forecasting/ccrfcd-gauge-grids/data"
 DST="data"
 
-rsync -a --progress "$SRC"/ "$DST"/ > "$EXP_NAME.out" 2>&1 &
+python scripts/add_hrr_env_params_v2.py > "$EXP_NAME.out" 2>&1 &
 
 # ---------------------------------------
 exit
